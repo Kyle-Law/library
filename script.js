3f3fd859194a1ad206ba3673e4539b4a006cb06f
kyle-law <junwei_1121@hotmail.com>
2020-08-12 05:19:04 +0800

    Adding book button to DOM

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let myLibrary = [];
+const btnContainer = document.getElementById("btn-container");
+const bookShelf = document.getElementById("book-shelf");
 
 function Book(title, author, price, status) {
   this.title = title;
@@ -7,6 +9,20 @@ function Book(title, author, price, status) {
   this.status = status;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
+
+function addBookBtn() {
+  const btn = document.createElement("button");
+  btn.setAttribute("id", "add-book");
+  btn.classList.add("add-book");
+  btn.textContent = "Add Book";
+  btnContainer.appendChild(btn);
+}
+
+function render(array) {
+  addBookBtn();
+}
+
+render(myLibrary);
