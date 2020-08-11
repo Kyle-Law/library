3e41bce30354b3ae160780a37e6b8b1a8bdfa720
kyle-law <junwei_1121@hotmail.com>
2020-08-12 04:56:51 +0800

    Setup html and basic styling

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(title, author, price, status) {
+  this.title = title;
+  this.author = author;
+  this.price = price;
+  this.status = status;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
