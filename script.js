a21a4ece1cb2d13f7c4aabf19de6d22226829896
kyle-law <junwei_1121@hotmail.com>
2020-08-12 17:39:57 +0800

    Refinement

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variables
 let myLibrary = [];
 const btnContainer = document.getElementById("btn-container");
 const bookShelf = document.getElementById("book-shelf");
@@ -31,7 +32,7 @@ function storeBook(e) {
   const price = bookPriceEl.value;
   const status = bookStatusEl.value;
 
-  // Set bookmark object, add to array
+  // Set book object, add to array
   const book = new Book(title, author, price, status);
   addBookToLibrary(book);
   // Set bookmarks in localStorage, fetch, reset input fields
@@ -44,17 +45,6 @@ function storeBook(e) {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
-
-// function addBookBtn() {
-//   const btn = document.createElement("button");
-//   btn.setAttribute("id", "show-modal");
-//   btn.classList.add("add-book");
-//   btn.textContent = "Add Book";
-//   btnContainer.appendChild(btn);
-//   btn.addEventListener("click", () => {
-//     console.log("adding book...");
-//   });
-// }
 
 function retriveBooks() {
   if (localStorage.getItem("myLibrary")) {
@@ -73,25 +63,36 @@ function appendChildren(parent, children) {
   children.forEach((child) => parent.appendChild(child));
 }
 
+function removeBook() {
+  bookShelf.removeChild(bookItem);
+  myLibrary = myLibrary.filter((b) => b !== book);
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+}
+
 function render() {
   // Remove all book shelf elements
   bookShelf.textContent = "";
   retriveBooks();
   myLibrary.forEach((book, index) => {
+    // Book Item
     const bookItem = document.createElement("div");
     bookItem.classList.add("book-item");
     bookItem.setAttribute("id", "book-item");
+    // Book Title
     const title = document.createElement("h1");
     title.classList.add("title");
+    title.textContent = book.title;
+    // Book Author
     const author = document.createElement("h2");
     author.classList.add("author");
+    author.textContent = book.author;
+    // Book Price
     const price = document.createElement("p");
     price.classList.add("price");
-    title.textContent = book.title;
-    author.textContent = book.author;
     price.textContent = `$ ${book.price}`;
     const actionContainer = document.createElement("div");
     actionContainer.classList.add("action-container");
+    // Read Button
     const readBtn = document.createElement("button");
     if (book.status === "Read") {
       readBtn.textContent = "Unread";
@@ -100,14 +101,16 @@ function render() {
     }
     readBtn.setAttribute("id", "read");
     readBtn.classList.add("read");
+    // Remove Button
     const removeBtn = document.createElement("button");
     removeBtn.textContent = "Remove";
     removeBtn.setAttribute("id", "remove");
     removeBtn.classList.add("remove");
+    // Append Nodes
     appendChildren(actionContainer, [readBtn, removeBtn]);
     appendChildren(bookItem, [title, author, price, actionContainer]);
     bookShelf.appendChild(bookItem);
-    // Add Event Listeners
+    // Add Event Listeners for Read and Remove buttons
     removeBtn.addEventListener("click", () => {
       bookShelf.removeChild(bookItem);
       myLibrary = myLibrary.filter((b) => b !== book);
